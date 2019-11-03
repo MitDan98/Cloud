d222a4f401f4d501aa2efd546fb81308ab852ee0
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-11-03 22:15:10 +0200

    Create resource-group.sh

---
+++ Script/resource-group.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Resource-group
+#Author: Dan Mititi 
+#prototype
+az group create --name default --location euwest
+if ($? -ne 0); then
+ echo "cannot been create"
+ exit 1
+else 
+ echo " group was created"
+fi
