83894235dab2af10cc2271e26190144615eebd1b
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-11-11 14:43:34 +0200

    Update Key.sh

--- Script/Key/Key.sh
+++ Script/Key/Key.sh
@@ -4,7 +4,7 @@
 #Cloud -encrypt
 $random = head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''
 az keyvault key create --name "myKEK" --vault-name $random --kty RSA-HS
-if[ $? -ne 0]; then
+if [ $? -ne 0]; then
  echo "The key cannot been creat..."
  exit 1
 else 
