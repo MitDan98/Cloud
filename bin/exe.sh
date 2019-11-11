12703f806677dfd4c4da597d3b35f7f4c89e219d
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-11-11 16:00:25 +0200

    Update exe.sh

--- bin/exe.sh
+++ bin/exe.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #exe
 #start-encrypt
-sudo chmod 111 Cloud/script/VM/VMLinux.sh
+sudo chmod 111 Cloud/Script/VM/VMLinux.sh
 if [ $? -ne 0 ] ; then
  echo " Cannot change mod ..."
  exit 1
 fi
-sudo chmod 111 Cloud/script/VM/VMWindows.sh
+sudo chmod 111 Cloud/Script/VM/VMWindows.sh
 if [ $? -ne 0 ] ; then
  echo " Cannot change mod ..."
  exit 1
 fi
-sudo chmod 111 Cloud/script/Resource group/resource-group.sh
+sudo chmod 111 Cloud/Script/Resource group/resource-group.sh
 if [ $? -ne 0 ] ; then
  echo " Cannot change mod ..."
  exit 1
@@ -20,7 +20,7 @@ sudo chmod 111 Cloud/script/Key/Key.sh
 if [ $? -ne 0 ] ; then
  echo " Cannot change mod ..."
  exit 1
- sudo chmod 111 Cloud/script/Key/encrypt enable.sh
+ sudo chmod 111 Cloud/Script/Key/encrypt enable.sh
  if [ $? -ne 0 ] ; then
   echo " Cannot change mod ..."
   exit 1
