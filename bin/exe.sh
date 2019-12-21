ac743aeff90b80717b67d5c7db4ba9ea7f86bcda
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-12-21 12:32:35 +0200

    Update exe.sh

--- bin/exe.sh
+++ bin/exe.sh
@@ -2,11 +2,11 @@
 #exe
 #start-encrypt
 sudo cd ..
-if [ $? -ne 0]; then
+if [ $? -ne 0] ; then
  echo "You shell no pass"
 fi
 sudo cd Cloud/Script/VM/
-if [ $? -ne 0]; then
+if [ $? -ne 0] ; then
  echo "You shell no pass"
 fi
 sudo chmod 111 VMLinux.sh
@@ -20,11 +20,11 @@ if [ $? -ne 0 ] ; then
  exit 1
 fi
 sudo cd ~
-if [ $? -ne 0]; then
+if [ $? -ne 0] ; then
  echo "You shell no pass"
 fi
 sudo cd Cloud/Scripts/Resource group/
-if [ $? -ne 0]; then
+if [ $? -ne 0] ; then
  echo "You shell no pass"
 fi
 sudo chmod 111 resource-group.sh
@@ -33,11 +33,11 @@ if [ $? -ne 0 ] ; then
  exit 1
 fi
 sudo cd ~
-if [ $? -ne 0]; then
+if [ $? -ne 0] ; then
  echo "You shell no pass"
 fi
 sudo cd Cloud/Scrips/key
-if [ $? -ne 0]; then
+if [ $? -ne 0 ] ; then
  echo "You shell no pass"
 fi
 sudo chmod 111 Key.sh
@@ -48,16 +48,7 @@ if [ $? -ne 0 ] ; then
  if [ $? -ne 0 ] ; then
   echo " Cannot change mod ..."
   exit 1
- fi
 else
  echo "All work with succes"
 
-Sudo cd ~
-if [ #? -ne 0]; then
- echo "not working..."
-
-Else
- echo "ok"
-
-Sudo .
 
