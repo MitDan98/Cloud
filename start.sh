b2f1a904f42b0d6828d0bad57bd67b0f18f3f27c
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2020-01-06 21:42:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -27,10 +27,8 @@ sudo chmod 111 encryptenable.sh
 else
  echo "All work with succes"
 fi
-sudo ./VMLinux.sh
-sudo ./VMWindows.sh
-sudo ./resource-group.sh
-sudo ./key.sh
-sudo ./encryptenable.sh
-exti 1
-fi
+./VMLinux.sh
+./resource-group.sh
+./key.sh
+./encryptenable.sh
+echo "VM and key was created and your machine is encrypted"
